6a0eb0a57b89dbb1acbd1654e0646730c8b2c8a0
Jenifar0909 <jenifarb2006@gmail.com>
2026-01-03 20:04:36 +0530

    PrintDay

---
+++ PrintDay.java
@@ -0,0 +1,26 @@
+package SelectionPrograms;
+
+public class PrintDay {
+	public static void main(String[] args) {
+		int day = 3;
+		switch (day) {
+		case 1:
+			System.out.println("monday");
+			break;
+		case 2:
+			System.out.println("tuesday");
+			break;
+		case 3:
+			System.out.println("wednesday");
+			break;
+		case 4:
+			System.out.println("thursday");
+			break;
+		case 5:
+			System.out.println("friday");
+			break;
+		default:
+			System.out.println("Invalid day");
+		}
+	}
+}
